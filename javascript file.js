32cbead6ab252ab7780093ddb588c066cadbfd08
maheshtambade <120250169+maheshtambade@users.noreply.github.com>
2022-12-10 19:09:17 +0530

    Add files via upload

---
+++ javascript file.js
@@ -0,0 +1,9 @@
+     function hide(){
+		document.querySelector('.menublock').style.display = "none";
+	}
+	 
+
+	 function show(){
+	 	document.querySelector('.menublock').style.display = "block";
+
+	 }
